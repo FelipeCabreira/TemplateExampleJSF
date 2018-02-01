b0bc392776bf0b3450d5e1893caeaec5c62d2a31
Allan Felipe Assis Moreira <allan.moreira@pmpa.ad>
2018-02-01 15:00:45 -0200

    Menu lateral + menu de login + conteúdo básico página + Ícone aba

---
+++ src/main/java/util/NomeSistema.java
@@ -0,0 +1,19 @@
+package util;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class NomeSistema {
+    private static final String nomeAbreviado = "GTEL";
+    private static final String nomeCompleto = "Sistema de Gestão da Telefonia Móvel";
+
+    public String getNomeAbreviado(){
+        return nomeAbreviado;
+    }
+
+    public static String getNomeCompleto() {
+        return nomeCompleto;
+    }
+}
